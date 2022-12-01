0b0a647eb87c1fc8f5d5422843ac46524433f582
xtiaaaan <nico.angus3@gmail.com>
2022-12-01 17:04:52 -0500

    Made changes

--- RPI/recycle_detect.py
+++ RPI/recycle_detect.py
@@ -125,11 +125,11 @@ while True:
 	# loop over the results
 	for r in objs:
 		# extract the bounding box and box and predicted class label
-		bbox = obj.bbox.scale(scale_x, scale_y)
+		bbox = r.bbox.scale(scale_x, scale_y)
 		x0, y0 = int(bbox.xmin), int(bbox.ymin)
 		x1, y1 = int(bbox.xmax), int(bbox.ymax)
-		percent = int(100 * obj.score)
-		label = '{}% {}'.format(percent, labels.get(obj.id, obj.id))
+		percent = int(100 * r.score)
+		label = '{}% {}'.format(percent, labels.get(r.id, r.id))
 		
 		# center coordinates of object detected
 		centerX = ((x1 - x0) // 2) + x1
